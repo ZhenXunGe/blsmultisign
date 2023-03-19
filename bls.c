cb6e8cf37814d223576b1cc3bb14f20939a15a1c
Sinka <xgao@zoyoe.com>
2023-03-19 19:31:05 +0800

    add initial bls imp

---
+++ bls.c
@@ -0,0 +1,30 @@
+#include <stdint.h>
+#include <bls.h>
+
+uint64_t g1[17 * 512];
+uint64_t gsum[17];
+uint64_t g2[33];
+uint64_t gt[96];
+
+__attribute__((visibility("default")))
+int zkmain() {
+    uint32_t len = (uint32_t)wasm_input(1);
+    for (int i=0;i<len;i++) {
+       for (int j=0; j<16; j++) { 
+         g1[i*17 + j] = wasm_input(0);
+       }
+       g1[i*17 + 16] = 0;
+    }
+
+    for (int i=0; i<32; i++) {
+        g2[i] = wasm_input(0);
+    };
+    g2[32] = 0;
+
+    for (int i=0; i<96; i++) {
+	gt[i] = wasm_input(0);
+    };
+    blssum(len, g1, gsum);
+    blspair(g1, g2, gt);
+    return 1;
+}
